e5e326e94eeb30ffaaf0d62fe04a06c0157b7e34
sina <can.ajj@gmail.com>
2025-08-29 18:05:04 +0330

    almost finished project

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import PropTypes from "prop-types";
 
 const containerStyles = {
   marginTop: "15px",
@@ -17,46 +16,43 @@ export default function StarRating({
   color = "#fcc419",
   size = 48,
   className = "",
+  rating = 0, // controlled rating
+  onSetRating = () => {}, // callback
 }) {
-  const [rating, setRating] = useState(1);
   const [tempRating, setTempRating] = useState(0);
-  const handleRating = function (rating) {
-    setRating(rating);
+
+  const handleClick = (r) => {
+    onSetRating(r); // send selected rating to parent
   };
+
   const textStyle = {
     color,
     fontSize: `${size / 1.5}px`,
     lineHeight: "1",
     margin: "0",
   };
+
   return (
     <div style={containerStyles} className={className}>
       <div style={starContainerStyles}>
-        {Array.from({ length: maxRating }, (_, i) => {
-          return (
-            <Star
-              key={i}
-              onRating={() => handleRating(i + 1)}
-              onHoverIN={() => setTempRating(i + 1)}
-              onHoverOut={() => setTempRating(0)}
-              full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
-              color={color}
-              size={size}
-            />
-          );
-        })}
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            onRating={() => handleClick(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            color={color}
+            size={size}
+          />
+        ))}
       </div>
       <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
-StarRating.propTypes = {
-  maxRating: PropTypes.number,
-  color: PropTypes.string,
-  size: PropTypes.number,
-  className: PropTypes.string,
-};
-const Star = function ({ onRating, full, onHoverIN, onHoverOut, color, size }) {
+
+const Star = function ({ onRating, full, onHoverIn, onHoverOut, color, size }) {
   const starStyle = {
     width: `${size}px`,
     height: `${size}px`,
@@ -64,12 +60,10 @@ const Star = function ({ onRating, full, onHoverIN, onHoverOut, color, size }) {
   };
   return (
     <span
-      style={starStyle}
-      onClick={() => {
-        onRating();
-      }}
-      onMouseEnter={onHoverIN}
+      onClick={onRating}
+      onMouseEnter={onHoverIn}
       onMouseLeave={onHoverOut}
+      style={starStyle}
     >
       {full ? (
         <svg
@@ -90,7 +84,7 @@ const Star = function ({ onRating, full, onHoverIN, onHoverOut, color, size }) {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth="{2}"
+            strokeWidth={2}
             d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
           />
         </svg>
@@ -98,35 +92,3 @@ const Star = function ({ onRating, full, onHoverIN, onHoverOut, color, size }) {
     </span>
   );
 };
-/*
-FULL STAR
-
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 20 20"
-  fill="#000"
-  stroke="#000"
->
-  <path
-    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-  />
-</svg>
-
-
-EMPTY STAR
-
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="#000"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="{2}"
-    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-  />
-</svg>
-
-*/
